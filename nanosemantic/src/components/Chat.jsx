50c5fc7e18945527311221d3ce56cd721ec3ece5
Игорь <applecake@inbox.ru>
2019-09-10 19:18:16 +0300

    добавил приветственное сообщение

--- nanosemantic/src/components/Chat.jsx
+++ nanosemantic/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useRef } from 'react';
+import React, { Component } from 'react';
 import style from '../stylesheets/chat.module.css';
 import Message from './Message';
 
@@ -14,7 +14,9 @@ class Chat extends Component {
   }
 
   componentDidMount = async () => {
-    const resp = await fetch(`https://biz.nanosemantics.ru/api/2.1/json/Chat.init`, {
+    const euid = '00b2fcbe-f27f-437b-a0d5-91072d840ed3';
+
+    const resp1 = await fetch(`https://biz.nanosemantics.ru/api/2.1/json/Chat.init`, {
       method: 'POST',
       header: {
         'Accept': 'application/json',
@@ -22,18 +24,34 @@ class Chat extends Component {
       },
       body: JSON.stringify(this.state.uuid),
     })
-    const data = await resp.json();
-    this.setState({ cuid: data });
+    const cuid = await resp1.json();
+    this.setState({ cuid: cuid.result.cuid });
+
+    const welcome = {
+      cuid: cuid.result.cuid,
+      euid: euid,
+    }
+    const resp2 = await fetch(`https://biz.nanosemantics.ru/api/2.1/json/Chat.event`, {
+      method: 'POST',
+      header: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(welcome),
+    })
+    const welcomeMessage = await resp2.json();
+    this.setState({
+      history: [...this.state.history, welcomeMessage.result.text.value],
+    });
   }
 
-  onChange = async (e) => {
+  changeInputContent = async (e) => {
     await this.setState({ clientMessage: e.target.value });
-    console.log(this.state);
   }
 
   submit = async () => {
     const chatRequest = {
-      cuid: this.state.cuid.result.cuid,
+      cuid: this.state.cuid,
       text: this.state.clientMessage,
     }
     const resp = await fetch(`https://biz.nanosemantics.ru/api/2.1/json/Chat.request`, {
@@ -48,32 +66,22 @@ class Chat extends Component {
     this.setState({
       history: [...this.state.history, this.state.clientMessage, data.result.text.value],
     });
-    console.log(data.result.text.value)
-  }
 
-  scroll = (e) => {
-    e.scrollTop = e.offsetHeight;
-    console.log('hui');
+    let chatWindow = this.refs.chatWindow;
+    chatWindow.scrollTop = 2**99; // нормального решения не придумал
   }
-
-  scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)
 
   render() {
-
-    const myRef = useRef(null);
-    const executeScroll = () => this.scrollToRef(myRef);
-
     return (
       <div>
-        <div className={style.chat} onChange={this.scroll}>
+        <div className={style.chat} ref='chatWindow'>
           {this.state.history.map((el, index) => {
-            return <Message ref={myRef} text={el} key={index} index={index}/>
+            return <Message text={el} key={index} index={index}/>
           })}
         </div>
         <div className={style.messageContainer}>
-          <input onChange={this.onChange} className={style.input_chat}></input>
+          <input onChange={this.changeInputContent} className={style.input_chat}></input>
           <button onClick={this.submit} className={style.buttonSubmit_chat}>Отправить</button>
-          <button onClick={executeScroll} className={style.buttonSubmit_chat}>Прокрутить</button>
         </div>
       </div>
     )
